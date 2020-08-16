7abc49b7546aa51e9a4a7550bd97efb88102bbf6
paola pinero <ppaola1409@gmail.com>
2020-08-16 18:41:20 -0400

    agregando funcion lib dynamo

--- models/DynamoBase.js
+++ models/DynamoBase.js
@@ -24,6 +24,17 @@ class DynamoBase {
         });
     }
 
+    removeEmptyStringElements(obj) {
+        for (var prop in obj) {
+            if (typeof obj[prop] === 'object') {// dive deeper in
+                this.removeEmptyStringElements(obj[prop]);
+            } else if (obj[prop] === '') {// delete elements that are empty strings
+                delete obj[prop];
+            }
+        }
+        return obj;
+      }
+
 }
 
 module.exports = DynamoBase;
